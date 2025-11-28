ef8164b08aeffd45385f59d1d47ffe92ebd7b5f4
Santiago Mansilla <mansilla.santiago@gmail.com>
2025-11-27 22:31:59 -0300

    Añadir script contador del 1 al 10

---
+++ contador.py
@@ -0,0 +1,8 @@
+# Script sencillo que imprime los números del 1 al 10 Santiago Mansilla
+
+def contar_hasta_diez():
+    for i in range(1, 11):
+        print(i)
+
+if __name__ == "__main__":
+    contar_hasta_diez()
